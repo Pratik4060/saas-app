fa0ceddc0014383ca11de54ffc16fc31916caace
pratik <raut70856@gmail.com>
2025-08-06 13:42:17 +0530

    Project Setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from '@/components/ui/button'
+const Page = () => {
+  return (
+    <>   <h1 className='text-2xl underline'> Welcome to my saas App</h1>
+   <Button>  let start</Button> 
+  </>
+ 
+  )
+}
+
+export default Page
