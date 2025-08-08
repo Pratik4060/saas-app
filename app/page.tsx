dc33f07c09fe387defc7dd0652ade39b3ddf7c55
pratik <raut70856@gmail.com>
2025-08-08 13:52:49 +0530

    build a form and setup a dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import {Button} from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CTA from '@/components/CTA'
+import CompanionsList from '@/components/CompanionsList'
+import { recentSessions } from '@/constants'
 const Page = () => {
   return (
-    <>   <h1 className='text-2xl underline'> Welcome to my saas App</h1>
-   <Button>  let start</Button> 
-  </>
+    <main>  
+     <h1 className='text-2xl underline'> Popular Companions</h1>
+       <section className='home-section'>
+        <CompanionCard 
+         id="123"
+         name="Neura the branly"
+         subject="AI Companion"
+         duration= {30}
+         color="blue"
+          topic="Learn AI with Neura"
+
+        />
+        <CompanionCard
+          id = "456"
+          name="Techie the Tutor"
+          subject="Tech"
+          duration={45}
+          color="green"
+          topic="Master Web Development"
+        />
+        <CompanionCard 
+         id = "789"
+          name="Math Wizard"
+          subject="Math"
+          duration={60}
+          color="yellow"
+          topic="Conquer Calculus"
+          
+         />
+       </section>
+
+       <section className='home-section'>
+     <CompanionsList
+        title="Recently Completed Sessions"
+        companions={recentSessions}
+     />
+        <CTA/>         
+       </section>
+
+  </main>
  
   )
 }
